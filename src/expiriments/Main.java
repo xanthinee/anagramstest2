e44c6fe443c145856b185fdeed77b58a64bc4303
xanthine <xanthine2002@gmail.com>
2022-05-18 12:25:23 +0400

    some minor changes caused by Logger

--- src/expiriments/Main.java
+++ src/expiriments/Main.java
@@ -1,10 +1,12 @@
 package expiriments;
 
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
         WorkingPart str = new WorkingPart();
-        System.out.println(str.stringReverse("art3m sm1rn0v  "));
+        out.println(str.stringReverse("art3m sm1rn0v  "));
     }
 }
